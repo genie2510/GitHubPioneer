7715abc3142577aed5e06c2b3192337fa53c46ce
routra01 <ravikumar.routhu.consultant@nielsen.com>
2017-04-13 12:16:51 +0530

    Modified to change artifact id in pom.xml and added comments

--- LambdaDen/src/main/java/com/amazonaws/lambda/demo/MultipleFunctionHandler.java
+++ LambdaDen/src/main/java/com/amazonaws/lambda/demo/MultipleFunctionHandler.java
@@ -3,6 +3,10 @@ package com.amazonaws.lambda.demo;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
+/**
+ * 	@author ravi.routhu
+ *	Handles Lambda request
+ */
 public class MultipleFunctionHandler implements RequestHandler<String, String> {
 
     public String handleRequest(String input, Context context) {
