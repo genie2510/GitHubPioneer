4e18676384b66ae2362cd034d7f0e85330205622
genie2510 <raveelucky@gmail.com>
2024-10-21 23:14:57 +0530

    Modified to introduce eureka server

---
+++ spring-genie/src/main/java/com/tutorials/demo/SpringGenieApplication.java
@@ -0,0 +1,13 @@
+package com.tutorials.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGenieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGenieApplication.class, args);
+	}
+
+}
---
+++ spring-genie/src/test/java/com/tutorials/demo/SpringGenieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorials.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGenieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
