eda528db1bee2e6ca4b3f540e5c7263f7f235743
genieGit <raveelucky@gmail.com>
2024-11-05 17:23:02 +0530

    Modified to introduce gateway-server

---
+++ spring-genie/gateway-server/src/main/java/com/tutorials/spring_genie_gateway/SpringGenieGatewayApplication.java
@@ -0,0 +1,22 @@
+package com.tutorials.spring_genie_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SpringGenieGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGenieGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+	    return builder.routes().build();
+	}
+}
